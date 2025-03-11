ebea6950809a1f24d0ee7f102e3e2023bdd89ac4
Chanh <chanhhoang99@gmail.com>
2025-03-11 16:25:57 +0700

    [taste]: Avoid calling service when navigation is not ready

--- nav2_simple_commander/nav2_simple_commander/robot_navigator.py
+++ nav2_simple_commander/nav2_simple_commander/robot_navigator.py
@@ -489,11 +489,11 @@ class BasicNavigator(Node):
 
     def waitUntilNav2Active(self, navigator='bt_navigator', localizer='amcl'):
         """Block until the full navigation system is up and running."""
-        if localizer != 'robot_localization':  # non-lifecycle node
-            self._waitForNodeToActivate(localizer)
-        if localizer == 'amcl':
-            self._waitForInitialPose()
-        self._waitForNodeToActivate(navigator)
+        # if localizer != 'robot_localization':  # non-lifecycle node
+        #     self._waitForNodeToActivate(localizer)
+        # if localizer == 'amcl':
+        #     self._waitForInitialPose()
+        # self._waitForNodeToActivate(navigator)
         self.info('Nav2 is ready for use!')
         return
 
